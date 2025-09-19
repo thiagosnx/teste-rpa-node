8648a7aa7b2ec2f6a3dc672fe0ad5cfffd69618b
thiagosnx <thiagosantanacq@gmail.com>
2025-09-19 16:11:53 -0300

    fix: fechando modal so apos salvar csv

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ async function getFilmes() {
     .setChromeOptions(new chrome.Options()) 
     .build();
 
+
+  const filePath = path.join(__dirname, "filmes_imdb.csv");
+  const header = ["TITULO", "ANO", "DURACAO", "NOTA", "CLASSIFICACAO", "SINOPSE"].join(";") + "\n";
+
+  if (!fs.existsSync(path.dirname(filePath))) fs.mkdirSync(path.dirname(filePath), { recursive: true });
+
+  if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, header, { encoding: "utf-8" });
+
   try {
     
     await driver.get("http://www.google.com.br/");
@@ -29,52 +37,54 @@ async function getFilmes() {
 
     
     const filmes = await driver.findElements(By.css(".ipc-metadata-list-summary-item"));
-    const resultados = [];
 
-    for (let i = 0; i < filmes.length; i++) {
-      const f = filmes[i];
+    for (let i = 0; i < Math.min(filmes.length, 5); i++) {
+      try {
+        const f = filmes[i];
 
-      
-      const sinopseBtn = await f.findElement(By.css(".ipc-icon-button.li-info-icon.ipc-icon-button--base.ipc-icon-button--onAccent2"));
-      await driver.actions({ bridge: true }).move({ origin: sinopseBtn }).perform();
-      await driver.wait(until.elementIsVisible(sinopseBtn), 50000);
-      await sinopseBtn.click();
+        const sinopseBtn = await f.findElement(By.css(".ipc-icon-button.li-info-icon.ipc-icon-button--base.ipc-icon-button--onAccent2"));
+        await driver.actions({ bridge: true }).move({ origin: sinopseBtn }).perform();
+        await driver.wait(until.elementIsVisible(sinopseBtn), 50000);
+        await sinopseBtn.click();
 
-      const sinopseElement = await driver.wait(
-        until.elementLocated(By.css(".sc-717a9add-2.jPYKsd")),
-        50000
-      );
-      const sinopse = await sinopseElement.getText();
+        const sinopseElement = await driver.wait(
+          until.elementLocated(By.css(".sc-717a9add-2.jPYKsd")),
+          50000
+        );
+        const sinopse = await sinopseElement.getText();
 
-      
+       
+
+        const titulo = await f.findElement(By.css(".ipc-title__text.ipc-title__text--reduced")).getText();
+        const nota = await f.findElement(By.css(".ipc-rating-star--rating")).getText();
+        const spans = await f.findElements(By.css(".sc-15ac7568-7.cCsint.cli-title-metadata-item"));
+
+        const ano = await spans[0].getText();
+        const duracao = await spans[1].getText();
+        const classificacao = spans.length > 2 ? await spans[2].getText() : "N/I";
+
+        const linha = [
+          titulo,
+          ano,
+          duracao,
+          nota,
+          classificacao,
+          `"${sinopse.replace(/"/g, '""')}"`
+        ].join(";") + "\n";
+
+        fs.appendFileSync(filePath, linha, "utf-8");
+
       const fechaSinopseBtn = await driver.findElement(By.css(".ipc-promptable-base__close"));
       await fechaSinopseBtn.click();
       await driver.wait(until.stalenessOf(sinopseElement), 50000);
+      } catch (err) {
+        console.error(`Erro: ${i}:`, err);
+      }
 
       
-      const titulo = await f.findElement(By.css(".ipc-title__text.ipc-title__text--reduced")).getText();
-      const nota = await f.findElement(By.css(".ipc-rating-star--rating")).getText();
-      const spans = await f.findElements(By.css(".sc-15ac7568-7.cCsint.cli-title-metadata-item"));
-
-      const ano = await spans[0].getText();
-      const duracao = await spans[1].getText();
-      const classificacao = spans.length > 2 ? await spans[2].getText() : "N/I";
-
-      resultados.push({ titulo, ano, duracao, nota, classificacao, sinopse });
     }
 
-    
-    const dataExcel = [
-      ["TITULO", "ANO", "DURACAO", "NOTA", "CLASSIFICACAO", "SINOPSE"],
-      ...resultados.map(r => [r.titulo, r.ano, r.duracao, r.nota, r.classificacao, r.sinopse])
-    ];
 
-    
-    const buffer = xlsx.build([{ name: "Filmes", data: dataExcel }]);
-    const filePath = path.join(__dirname, "filmes_imdb.xlsx");
-    fs.writeFileSync(filePath, buffer);
-
-    
     moveFile(filePath, "uploads");
 
   } finally {
